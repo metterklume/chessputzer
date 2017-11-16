621d3c3a3c3329c0969d0cfa847abe845c63dae2
A Roy <gophergoon@gmail.com>
2017-11-16 17:31:43 +0100

    Main code
    Main functions

---
+++ putzpresets.py
@@ -0,0 +1,273 @@
+from __future__ import division
+import os
+import numpy as np
+import PIL.Image as Image
+import scipy.signal
+import cv2
+import PIL.ImageFilter as ImageFilter
+from IPython.display import display
+#import matplotlib.pyplot as plt
+import glob
+from io import BytesIO
+from math import floor
+
+# pathboards = os.path.expanduser("~")+"/Dropbox/putz/boards3/"
+# pathlight = pathboards+'lightsquares/'
+# pathdark = pathboards + 'darksquares/'
+# pathavglight = pathboards+'pieceaverages/'+'lightsquares/'
+# pathavgdark =  pathboards+'pieceaverages/'+'darksquares/'
+pathtemps = os.path.expanduser("~")+"/Dropbox/putz/piecetemps/"
+
+pieces=['P','R','N','B','Q','K','p','r','n','b','q','k',' ']
+piecenames={' ':'zz'}
+for p in pieces[:6]:
+    piecenames[p] = 'w'+p.lower()
+for p in pieces[6:12]:
+    piecenames[p] = 'b'+p.lower()
+piecenamesrev={}
+for k,v in piecenames.items():
+    piecenamesrev[v] = k
+
+def nvec(v):
+    a = v.dot(v)
+    return v/np.sqrt(a)
+
+def norma(arr):
+    arr2=np.float32(arr.flatten())
+    return np.sqrt(arr2.dot(arr2))
+
+def skel(a):
+    acop = np.copy(a)
+    l = a.shape[0]
+    for i in range(l-1):
+        if acop[i] < acop[i+1]:
+            acop[i]=0
+    for i in range(l-1,1,-1):
+        if acop[i] <= acop[i-1]:
+            acop[i]=0
+    return acop
+
+def displayarray(a,rng=[0,255]):
+    ma,mi = rng[1],rng[0]
+    a = ((a-mi)/(ma-mi))*255
+    a = np.clip(a,0,255)
+    return Image.fromarray(np.uint8(a))
+
+def gaussim(a,m=6,sdev=2):
+    f = scipy.signal.gaussian(m,2)
+    gwin = np.zeros((m,m))
+    for i in range(m):
+        for j in range(m):
+            gwin[i,j] = f[i]*f[j]
+    return scipy.signal.convolve2d(a,gwin/gwin.sum(),mode='same')
+
+
+def probs(overs,nsets=3):
+    pl = np.array([max(overs[i:i+nsets]) for i in range(0,len(overs),nsets)])
+    return [(pieces[i],pl[i]) for i in np.argsort(pl)[::-1][:4]]
+
+def piecepredold(imarr,pset,ld):
+    nsets = len(pset)/12
+    imarr=np.where(imarr<100,0,imarr)
+    if ld == 1: #get rid of some of the background and resize
+        imarr = backfilter(imarr)
+    else: #just resize
+        imarr = cv2.resize(imarr,(32,32),interpolation=cv2.INTER_AREA)
+    imarr = np.float32(imarr)
+    if np.mean(imarr)<1:
+        return 12
+    imarr = imarr/norma(imarr)
+    overs = [overlap(p,imarr) for p in pset]
+    if max(overs) < .5:
+        return 12
+    else:
+        return int(np.argmax(overs)/nsets)
+    
+def piecepredebug(imarr,pset,ld):
+    nsets = len(pset)/12
+#    if blankpred(imarr,ld) == 1:
+#        return 12
+    imarr=np.where(imarr<100,0,imarr)
+    if ld == 1: #get rid of some of the background and resize
+        imarr = backfilter(imarr)
+    else: #just resize
+        imarr = cv2.resize(imarr,(32,32),interpolation=cv2.INTER_AREA)
+    imarr = np.float32(imarr)
+    if np.mean(imarr)<1:
+        print(empty)
+        return 12,[]
+    imarr = imarr/norma(imarr)
+    overs = [overlap(p,imarr) for p in pset]
+    if max(overs) < .4:
+        print('max too low')
+        return 12,overs
+    else:
+        return int(np.argmax(overs)/nsets),overs
+
+def pprobs(ima,ld=1):
+    _,ov=piecepredebug(ima,pbarrs,ld)
+    return probs(ov)
+
+def boardtofen(board):
+    board = board.split('/')
+    fen = ''
+    for row in board:
+        count=0
+        for p in row:
+            if p in pieces[:-1]:
+                if count>0:
+                    fen+=str(count)
+                fen += p
+                count=0
+            else:
+                count+=1
+        if count>0:
+            fen+=str(count)
+        fen += '/'
+    return fen[:-1]
+
+def houghbox(imarr,minfactor=.4,rho=1):
+    l,w = imarr.shape
+    tol = np.pi/20
+    edges = cv2.Canny(imarr,100,200,apertureSize = 3)
+    lines = cv2.HoughLines(edges,rho,np.pi/50,int(minfactor*l))
+    if lines is None:
+        return [],[],(0,0),(0,0)
+    #careful below! I had horizontal and vertical confused at first
+    #and of course it still works if the image is almost a square
+    vlines = np.sort([l[0] for [l] in lines if abs(l[1])<tol]).astype(np.int)
+    hlines = np.sort([l[0] for [l] in lines if abs(l[1]-np.pi/2)<tol]).astype(np.int)
+    #print(lines)
+    if len(vlines) <=1  or len(hlines) <= 1:
+        print("Not enough lines")
+        return hlines,vlines,(0,0),(0,0)
+    up,down = 0,len(hlines)-1
+    while hlines[down]-hlines[up] > l*7/8:
+        up+=1
+    up-=1
+    while hlines[down]-hlines[up] > l*7/8:
+        down-=1
+    if down<len(hlines)-1:
+        down+=1
+    #
+    left,right = 0,len(vlines)-1
+    while vlines[right]-vlines[left] > w*7/8:
+        left+=1
+    left-=1
+    while vlines[right]-vlines[left] > w*7/8:
+        right-=1
+    if right<len(vlines)-1:
+        right+=1
+    #print(hlines,up,down,vlines,left,right)
+    return hlines,vlines,(hlines[up],hlines[down]),(vlines[left],vlines[right])
+
+def splitboardhough(ima,minfactor=.33):
+    imablur = np.uint8(gaussim(ima,m=4))
+    _,_,(up,down),(left,right) = houghbox(imablur,minfactor=minfactor)
+    l,w = ima.shape
+    if down-up< l*3/4 or right-left < w*3/4:
+        return []
+    else:
+        xw,yw = int(round((down-up)/8)),int(round((right-left)/8))
+        print(xw,yw)
+        return [ima[up+j*xw:up+(j+1)*xw,left+i*yw:left+(i+1)*yw] for j in range(8) for i in range(8)]
+
+def splitboard(ima):
+    fudge = -2
+    l,w = ima.shape
+    xl,yl,_,_ = findlines(ima)
+    xlines,ylines = findlineset(xl,10),findlineset(yl,10)
+    if len(xlines)<2 or len(ylines)<2:
+        return [] #not enough lines
+    xwidth,ywidth = int(np.mean(np.diff(xlines))),int(np.mean(np.diff(ylines)))
+    if abs(xwidth*8 - w) > xwidth or abs(ywidth*8 - l)>ywidth:
+        return [] #lines too far apart/too close together to be one square
+    xstart = max(0,xlines[0]-floor((fudge+xlines[0])/xwidth)*xwidth)
+    ystart = max(0,ylines[0]-floor((fudge+ylines[0])/ywidth)*ywidth)
+    if xstart+7*xwidth > w:
+        np.pad(ima,(0,0,0,xstart+7*xwidth-w),mode='edge')
+    if ystart+7*ywidth > l:
+        np.pad(ima,(0,ystart+7*ywidth-l,0,0),mode='edge')
+    xlines = [xstart + n*xwidth for n in range(8)]
+    ylines = [ystart + n*ywidth for n in range(8)]
+    #print(xlines,ylines)
+    squares= [ima[j:j+xwidth,i:i+ywidth] for j in ylines for i in xlines]
+    return squares
+
+def showbounds(ima,minfactor=.4):
+    imc = np.copy(ima)
+    xl,yl,ylines,xlines=houghbox(imc,minfactor)
+    plt.imshow(imc)
+    for x in xl:
+        plt.axvline(x,color='b')
+    for y in yl:
+        plt.axhline(y,color='r')
+    return None
+
+def findlines(ima):
+    ima = gaussim(ima)
+    # find x and y gradients
+    xkern = [[-1.,0., 1.],[-1.,0., 1.],[-1.,0., 1.]]
+    ykern = [[1.,1.,1.],[0.,0.,0.],[-1.,-1.,-1.]]
+    imax = scipy.signal.convolve2d(ima,xkern)
+    imay = scipy.signal.convolve2d(ima,ykern)
+    #Use the cool trick from chessfenbot. Lines are alternating.
+    imax_pos = np.clip(imax,0,600)
+    imax_pos = np.sum(imax_pos,0)
+    imax_neg = np.clip(imax,-600,0)
+    imax_neg = np.sum(imax_neg,0)
+    # y lines
+    imay_pos = np.clip(imay,0,600)
+    imay_pos = np.sum(imay_pos,1)
+    imay_neg = np.clip(imay,-600,0)
+    imay_neg = np.sum(imay_neg,1)
+    # normalize and return
+    tt=(imax_pos*np.abs(imax_neg))
+    tt=tt/tt.max()
+    rr=(imay_pos*np.abs(imay_neg))
+    rr=rr/rr.max()
+    xlines = np.where(skel(tt)>.3)[0]
+    ylines = np.where(skel(rr)>.3)[0]
+    return xlines,ylines,tt,rr
+
+def findlineset(lines,mindiff):
+    #kludgy
+    l = len(lines)
+    count,start,dmax = 1,0,mindiff
+    linesmax=[]
+    for i in range(l-4):
+        for j in range(i+1,min(i+4,l)):
+            ltries = [lines[i]]
+            d = lines[j]-lines[i]
+            k = j
+            if d > mindiff:
+                restdiff = [abs(lines[r]-ltries[-1]-d) for r in range(k,l)]
+                while min(restdiff) < 5:
+                    ltries += [lines[k+np.argmin(restdiff)]]
+                    k = np.argmin(restdiff)+1
+                    restdiff = [abs(lines[r]-ltries[-1]-d) for r in range(k,l)]
+            if len(ltries)>count:
+                count,start,dmax,linesmax = len(ltries),i,d,ltries
+    return linesmax
+
+def piecetemp(pathbench):
+    benchfiles=[glob.glob(pathbench+piecenames[p]+'*png') for p in pieces[:12]]
+    ptemps = [Image.open(p).convert('L').resize((32,32),Image.ANTIALIAS) for pname in benchfiles for p in pname]
+    pbarrs = [np.bitwise_not(np.asarray(p,np.uint8)) for p in ptemps]
+    pbarrs = [np.float32(arr)/norma(arr) for arr in pbarrs]
+    pbarrs = [np.pad(arr,((8,8),(8,8)),mode='constant') for arr in pbarrs]
+    return pbarrs
+
+def cropim(imarr,margin=.1):
+    ima = np.copy(imarr)
+    assert(margin<1 and margin > 0)
+    l,w = ima.shape
+    cmargin=1-margin
+    ima[:int(margin*l)]=ima[int(cmargin*l):]=0
+    ima[:,:int(margin*w)]=ima[:,int(cmargin*w):]=0
+    return ima
+
+def predfile(fpath,ld):
+    imarr = np.asarray(Image.open(fpath),np.uint8)
+    imarr = np.bitwise_not(imarr)
+    return piecepred(imarr,pbarrs,ld)
---
+++ putzrefactor.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 31 16:37:09 2017
+
+@author: klein
+"""
+from __future__ import division
+import numpy as np
+import PIL.Image as Image
+import scipy.signal
+import cv2
+#from IPython.display import display
+import glob
+from os.path import basename
+import requests
+from io import BytesIO
+import sys
+if sys.platform == 'win32':
+    import PIL.ImageGrab as ImageGrab
+
+from putzpresets import pathtemps,pieces,piecenames
+from putzpresets import houghbox,splitboardhough,splitboard,boardtofen,norma
+
+class Board():
+    def __init__(self,*impath):
+        if impath:
+            self.im = Image.open(impath[0])
+        else:
+            self.im = ImageGrab.grabclipboard()
+        self.ima = np.asarray(self.im.convert("L"),dtype=np.uint8)
+        #self.imablur = np.uint8(gaussim(self.ima,m=4))
+        self.squares = splitboardhough(self.ima,minfactor=.4)
+#        if not self.squares:
+#            self.squares = splitboard(self.ima)
+        self.squares = [np.bitwise_not(s) for s in self.squares]
+        self.g = [-1]*64
+    #    
+    def getpieces(self,pbarrs):        
+        if not self.squares:
+            return ""
+        candidates = [[]]*64
+        candprobs  = [[]]*64
+        guesses = [-1]*64
+        #guesspos = [0]*64
+        #first pass
+        for i,s in enumerate(self.squares):
+            ld = (i + (i//8))%2
+            tg,to = piecepred(s,pbarrs,ld)
+            if tg[0] == 12:
+                guesses[i] = 12
+                candidates[i] = [12]
+            else:
+                #
+                #print(i,tg,to)
+                assert(len(tg)==len(to))
+                to = [x for x in to if x>.5]
+                tg = tg[len(tg)-len(to):]
+                #
+                assert(len(tg)==len(to))
+                guesses[i] = tg[-1]
+                candidates[i] = tg
+                candprobs[i]  = to
+        #
+        piecemax=[8,2,2,2,1,1,8,2,2,2,1,1,64]
+        def toomany(p,nextbest=True):
+            inds = [i for i,q in enumerate(guesses) if q==p]
+            if len(inds)<= piecemax[p]:
+                return 0 #no changes needed
+            for _ in range(piecemax[p]):
+                m = np.argmax([candprobs[ind][-1] for ind in inds])
+                inds.pop(m)
+            if not nextbest:
+                for q in inds:
+                    guesses[q] = 12
+            #
+            else:
+                for q in inds:
+                    candidates[q].pop(-1)
+                    candprobs[q].pop(-1)
+                    if candidates[q]:
+                        guesses[q] = candidates[q][-1]
+                    else:
+                        guesses[q] = 12
+            print("changed %d %s"%(len(inds),pieces[p]))
+            return len(inds)
+        #
+        #second pass, no pawns on the first or eighth ranks
+        for q in list(range(8))+list(range(56,64)):
+            if guesses[q] == 0 or guesses[q]==6:
+                candidates[q].pop(-1)
+                candprobs[q].pop(-1)
+                if candidates[q]:
+                    guesses[q] = candidates[q][-1]
+                else:
+                    guesses[q] = 12
+        #one king each
+        toomany(5,nextbest=False)
+        toomany(11,nextbest=False)
+        #one queen each
+        toomany(4,nextbest=True)
+        toomany(10,nextbest=True)
+        #two rooks
+        toomany(7,nextbest=False)
+        toomany(1,nextbest=True)
+        #two knights
+        toomany(8,nextbest=False)
+        toomany(1,nextbest=True)
+        #eight pawns
+        toomany(6,nextbest=False)
+        toomany(0,nextbest=True)
+        self.g = guesses
+        self.b=""
+        for i,s in enumerate(guesses):
+            if i%8 == 0 and i>0:
+                self.b+='/'
+            ld = (i + (i//8))%2 #light or dark square
+            self.b+= pieces[guesses[i]]
+        return boardtofen(self.b)
+
+def piecepred(imarr,pset,ld):
+    nsets = len(pset)//12
+    if ld == 1 and stripetest(imarr) > .15: #screen out blank images
+        return [12],[1]
+    if ld == 0 and blanktest(imarr) < 10:   #screen out blank images
+        return [12],[1]
+    imarr=np.where(imarr<100,0,imarr)
+    if ld == 1: #get rid of some of the striped background & resize
+        imarr = backfilter(imarr)
+    else:      #just resize
+        imarr = backfilter(imarr)
+        #imarr = cv2.resize(imarr,(32,32),interpolation=cv2.INTER_AREA)
+    imarr = np.float32(imarr)
+    if np.mean(imarr)<1:
+        return [12],[1]
+    imarr = imarr/norma(imarr)
+    overs = [overlap(p,imarr) for p in pset]
+    if max(overs) < .5: #not enough overlap with any piece
+        return [12],[1]
+    redovers = [max(overs[i:i+nsets]) for i in range(0,len(overs),nsets)]
+    topguesses = np.argsort(redovers)[-4:]
+    topovers   = [redovers[i] for i in topguesses]
+    return list(topguesses),list(topovers)
+
+def overlap(imarr1,imarr2):
+	imarr1,imarr2 = np.float32(imarr1),np.float32(imarr2)
+	overs = scipy.signal.convolve2d(imarr1,imarr2[::-1,::-1],mode='valid')
+	return overs.max()
+
+def stripefilter(ima):
+    l,w = ima.shape
+    imf = np.fft.fft2(ima)
+    for j in range(2,int(l/2)):
+        imf[j,j],imf[j,j+1],imf[j+1,j]=0,0,0
+        imf[l-j,w-j],imf[l-j,w-j-1],imf[l-j-1,w-j]=0,0,0
+    ima2 = np.real(np.fft.ifft2(imf))
+    ima2 = np.clip(ima2,0,255)
+    return ima2.astype(np.uint8)
+
+def pieceacc(pathtodir,piece,units,ld):
+    pnames = glob.glob(pathtodir+piecenames[piece]+"*")
+    if not pnames:
+        return (-1,[],[])
+    pims = [Image.open(pname) for pname in pnames]
+    parrs = [np.bitwise_not(np.asarray(im,np.uint8)) for im in pims]
+    results = [piecepred(imarr,units,ld) for imarr in parrs]
+    corr = [1 if i==pieces.index(piece) else 0 for i in results]
+    #corr2 = [1 if (i-pieces.index(piece))%6==0 else 0 for i in results]
+    wrongs = [results[i] for i,x in enumerate(corr) if x==0]
+    wrongnames = [pnames[i] for i,x in enumerate(corr) if x==0]
+    return sum(corr)/len(corr),wrongs,wrongnames
+
+
+def blanktest(ima):
+    '''return max of the central half of the ima'''
+    l,w = ima.shape
+    m = np.max(ima[int(l/4):int(3*l/4),int(w/4):int(3*w/4)])
+    return m
+
+def stripetest(ima):
+    '''check for stripe pattern in fft'''
+    imf = np.fft.fft2(ima)
+    x = -1
+    for i in (-1,0,1):
+        y = np.max(np.abs(imf.diagonal(i)[1:]))**2/np.abs(imf[0,0])**2
+        x = max(x,y)
+    return x
+
+def backfilter(im,finalshape=(32,32),passes=2):
+    #imcvg= cropim(im,margin=.08)
+    imcvg=im
+    imcvg= cv2.resize(imcvg,(128,128),interpolation = cv2.INTER_CUBIC)
+    #imcvg = cv2.GaussianBlur(imcvg,(11,11),0)
+    for _ in range(passes):
+        mask =np.zeros(imcvg.shape,np.uint8)
+        ee = cv2.Canny(imcvg,200,400,apertureSize = 3)
+        _,contours,_ = cv2.findContours(ee, 1, 2)
+        dashes = [c for c in contours if abs(cv2.minAreaRect(c)[2]+45)<8]
+        cv2.drawContours(mask,dashes,-1,255,4)
+        imclean = np.where(mask==0,imcvg,0)
+        imcvg = imclean
+    return cv2.resize(imclean,finalshape,interpolation=cv2.INTER_AREA)
+
+def fentoimg(fen):
+    fenurl='http://www.fen-to-image.com/image/'+fen
+    response = requests.get(fenurl)
+    img = Image.open(BytesIO(response.content))
+    return img
+
+### Load image templates 
+nsets = 3
+alltemps = np.load('pbarrs.npz').items()[0][1]
+numpieces = nsets * 12
+rows = alltemps.shape[0]//numpieces
+pbarrs = [alltemps[rows*i:rows*(i+1)][:] for i in range(numpieces)]
+assert(len(pbarrs) == numpieces)
